010540cd689eaad98dce441c5f0e75da21456279
htz4004 <htz4004@126.com>
2016-12-15 08:45:54 +0800

    star

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      <v-header></v-header>
+      <v-header :seller="seller"></v-header>
       <div class="tab">
         <div class="tab-item">
           <router-link to="/goods">商品</router-link>
@@ -16,10 +16,26 @@
   </div>
 </template>
 
-<script>
+<script type="text/esmascript-6">
   import header from './components/header/header.vue';
 
+  const ERR_OK = 0;
+
   export default {
+    data(){
+      return {
+        seller:{}
+      }
+    },
+    created(){
+      this.$http.get('/api/seller').then((response) => {
+        response = response.body;
+        if(response.errno === ERR_OK) {
+           this.seller = response.data;
+           console.log(this.seller);
+        }
+      })
+    },
     components: {
       'v-header': header
     }
@@ -27,11 +43,14 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/mixin.styl"
+
   .tab
     display:flex
     width:100%
     height:40px
     line-height:40px
+    border-1px(rgba(7,17,27,0.1))
     .tab-item
       flex:1
       text-align:center
--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-Vue.use(VueRouter);
+import VueResource from 'vue-resource';
 
 import App from './App';
 import goods from './components/goods/goods';
 import ratings from './components/ratings/ratings';
 import seller from './components/seller/seller';
 
+import './common/stylus/index.styl';
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
 
 let router = new VueRouter({
 	linkActiveClass:'active',
